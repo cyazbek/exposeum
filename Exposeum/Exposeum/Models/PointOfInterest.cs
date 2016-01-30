4ed8c36324a935d12e1dd7e8ee45af54b2dbe2a2
Maxwell Mowbray <mmowbray@mentlegen.com>
2016-01-30 18:18:02 -0500

    Added u, v, and label properties to POI class

--- Exposeum/Exposeum/Models/PointOfInterest.cs
+++ Exposeum/Exposeum/Models/PointOfInterest.cs
@@ -4,9 +4,38 @@ namespace Exposeum
 {
 	public class PointOfInterest
 	{
+		private float u;
+		private float v;
+		private String label;
+
 		public PointOfInterest ()
 		{
+			u = 0.0f;
+			v = 0.0f;
+		}
+
+		public PointOfInterest (float u, float v)
+		{
+			this.u = u;
+			this.v = v;
+		}
+
+		public string Label
+		{
+			set { this.label = value; }
+			get { return this.label; }
+		}
+
+		public string U
+		{
+			set { this.u = value; }
+			get { return this.u; }
+		}
+
+		public string V
+		{
+			set { this.v = value; }
+			get { return this.v; }
 		}
 	}
 }
-
