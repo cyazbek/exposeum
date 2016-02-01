ddb5ed77e287e509c6c19912e412f9c0f7f7082e
Olivier Brochu Dufour <olibd@hotmail.com>
2016-01-31 22:59:42 -0500

    BeaconActivity can now find beacons!!!

--- Exposeum/Exposeum/BeaconActivity.cs
+++ Exposeum/Exposeum/BeaconActivity.cs
@@ -23,12 +23,12 @@ namespace Exposeum
 	public class BeaconActivity : Activity, BeaconManager.IServiceReadyCallback
 	{
 
-		const int REQUEST_ENABLE_BLUETOOTH = 123321;
 		private BeaconManager beaconManager;
-		private EstimoteSdk.Region region;
+		private readonly EstimoteSdk.Region REGION = new EstimoteSdk.Region("rid", "B9407F30-F5F8-466E-AFF9-25556B57FE6D");
 		private bool beaconInRegion;
-		bool beaconsEnabled = false;
 		private TextView beaconContextualText;
+		private bool isRanging;
+		private readonly string CLASSNAME = typeof(BeaconActivity).Name;
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -37,22 +37,18 @@ namespace Exposeum
 			//View v = new BeaconView(Resource.Layout.Beacon);
 			SetContentView (Resource.Layout.Beacon);
 
+			//define UI bindings
 			beaconContextualText = FindViewById<TextView>(Resource.Id.textView1);
 
 
 			beaconManager = new BeaconManager (this);
 
-			//add event handler for when ranging
+			//add event handlers
 			beaconManager.Ranging += BeaconManagerRanging;
-
-
-
 			beaconManager.EnteredRegion += (sender, args) => setBeaconInRegion(true);
-
 			beaconManager.ExitedRegion += (sender, args) => setBeaconInRegion(false);
 
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
-			StartActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+
 		}
 
 
@@ -65,7 +61,10 @@ namespace Exposeum
 				return;
 			}
 
-			bool close = false;
+			Log.Debug("BeaconActivity", "Found {0} beacons.", e.Beacons.Count);
+			beaconContextualText.Text = "Found " + e.Beacons.Count + " beacon(s).";
+
+			/*bool close = false;
 			foreach (var beacon in e.Beacons)
 			{
 				var proximity = Utils.ComputeProximity(beacon);
@@ -79,48 +78,52 @@ namespace Exposeum
 				var accuracy = Utils.ComputeAccuracy(beacon);
 				if (accuracy > .06)
 					continue;
-
-				Log.Debug("BeaconActivity", "Found {0} beacons.", e.Beacons.Count);
-
-				beaconContextualText.Text = "Found " + e.Beacons.Count + "beacons.";
-
-			}
+			}*/
 		}
 
 		public void OnServiceReady()
 		{
-			if (region != null)
+			if (REGION == null)
 			{
-				beaconManager.StopRanging(region);
-				region = null;
+				throw new Exception("beacon reagion is null");
 			}
-
-			region = new EstimoteSdk.Region("rid", "B9407F30-F5F8-466E-AFF9-25556B57FE6D");
-			beaconManager.StartRanging(region);
-			beaconsEnabled = true;
+			try
+			{
+				if(checkBluetooth()){
+					beaconManager.StartRanging(REGION);
+					Log.Debug(CLASSNAME, "Looking for beacons in the region.");
+					isRanging = true;
+				}
+			}
+			catch (RemoteException e)
+			{
+				isRanging = false;
+				Log.Error(CLASSNAME, "Cannot start ranging, {0}", e);
+			}
+	
 		}
 
 		protected override void OnResume()
 		{
 			base.OnResume();
-			CheckBluetooth();
+			//CheckBluetooth();
 
-			//connect to 
-			if (region != null && beaconsEnabled)
-				beaconManager.Connect(this);
+			//connect to the beacon service
+			beaconManager.Connect(this);
 		}
 
 		protected override void OnPause()
 		{
-			base.OnPause();
-			if (beaconsEnabled)
+			// Make sure we disconnect from the beacon service.
+			if (isRanging)
 				beaconManager.Disconnect();
+			base.OnPause();
 		}
 
 		protected override void OnDestroy()
 		{
-			// Make sure we disconnect from the Estimote.
-			if (beaconsEnabled)
+			// Make sure we disconnect from the beacon service.
+			if (isRanging)
 				beaconManager.Disconnect();
 			base.OnDestroy();
 		}
@@ -129,29 +132,27 @@ namespace Exposeum
 			beaconInRegion = inRegion;
 		}
 
-		private void CheckBluetooth()
+		private bool checkBluetooth()
 		{
-			beaconContextualText.Visibility = ViewStates.Gone;
 			//Validation checks
 			if (!beaconManager.HasBluetooth || !PackageManager.HasSystemFeature(PackageManager.FeatureBluetoothLe))
 			{
-				beaconsEnabled = false;
+				return false;
 			}
 			else if (!beaconManager.IsBluetoothEnabled)
 			{
 				//bluetooth is not enabled
 				beaconContextualText.Text = "You must enable Bluetooth";
-				beaconsEnabled = false;
+				return false;
 			}
 			else if (!beaconManager.CheckPermissionsAndService())
 			{
 				beaconContextualText.Text = "There is an issues with bluetooth permission, make sure exposeum is authorized to use the Bluetooth.";
-				beaconsEnabled = false;
+				return false;
 			}
 			else
 			{
-				beaconsEnabled = true;
-				beaconContextualText.Visibility = ViewStates.Visible;
+				return true;
 			}
 		}
 
