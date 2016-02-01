ca6857072addaa06f3c1c0e0cc9aaab9d2db53c4
Olivier Brochu Dufour <olibd@hotmail.com>
2016-01-31 23:32:03 -0500

    Refactored the beacon finding functionalities of the Activity into their own class

--- Exposeum/Exposeum/BeaconActivity.cs
+++ Exposeum/Exposeum/BeaconActivity.cs
@@ -20,15 +20,12 @@ namespace Exposeum
 {
 	[Activity(Label = "@string/beacon_activity")]	
 	//in the future beacon discovery should probably be handled by a service or the Application android class
-	public class BeaconActivity : Activity, BeaconManager.IServiceReadyCallback
+	public class BeaconActivity : Activity
 	{
 
-		private BeaconManager beaconManager;
-		private readonly EstimoteSdk.Region REGION = new EstimoteSdk.Region("rid", "B9407F30-F5F8-466E-AFF9-25556B57FE6D");
-		private bool beaconInRegion;
 		private TextView beaconContextualText;
-		private bool isRanging;
-		private readonly string CLASSNAME = typeof(BeaconActivity).Name;
+		private BeaconFinder beaconFinder;
+
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -40,120 +37,29 @@ namespace Exposeum
 			//define UI bindings
 			beaconContextualText = FindViewById<TextView>(Resource.Id.textView1);
 
-
-			beaconManager = new BeaconManager (this);
-
-			//add event handlers
-			beaconManager.Ranging += BeaconManagerRanging;
-			beaconManager.EnteredRegion += (sender, args) => setBeaconInRegion(true);
-			beaconManager.ExitedRegion += (sender, args) => setBeaconInRegion(false);
+			beaconFinder = new BeaconFinder();
 
 
-		}
 
-
-		private void BeaconManagerRanging(object sender, BeaconManager.RangingEventArgs e)
-		{
-
-
-			if (e.Beacons == null)
-			{
-				return;
-			}
-
-			Log.Debug("BeaconActivity", "Found {0} beacons.", e.Beacons.Count);
-			beaconContextualText.Text = "Found " + e.Beacons.Count + " beacon(s).";
-
-			/*bool close = false;
-			foreach (var beacon in e.Beacons)
-			{
-				var proximity = Utils.ComputeProximity(beacon);
-
-				if (proximity != Utils.Proximity.Unknown)
-					close = true;
-
-				if (proximity != Utils.Proximity.Immediate)
-					continue;
-
-				var accuracy = Utils.ComputeAccuracy(beacon);
-				if (accuracy > .06)
-					continue;
-			}*/
-		}
-
-		public void OnServiceReady()
-		{
-			if (REGION == null)
-			{
-				throw new Exception("beacon reagion is null");
-			}
-			try
-			{
-				if(checkBluetooth()){
-					beaconManager.StartRanging(REGION);
-					Log.Debug(CLASSNAME, "Looking for beacons in the region.");
-					isRanging = true;
-				}
-			}
-			catch (RemoteException e)
-			{
-				isRanging = false;
-				Log.Error(CLASSNAME, "Cannot start ranging, {0}", e);
-			}
-	
 		}
 
 		protected override void OnResume()
 		{
 			base.OnResume();
-			//CheckBluetooth();
 
-			//connect to the beacon service
-			beaconManager.Connect(this);
 		}
 
 		protected override void OnPause()
 		{
-			// Make sure we disconnect from the beacon service.
-			if (isRanging)
-				beaconManager.Disconnect();
+
+
 			base.OnPause();
 		}
 
 		protected override void OnDestroy()
 		{
-			// Make sure we disconnect from the beacon service.
-			if (isRanging)
-				beaconManager.Disconnect();
+
 			base.OnDestroy();
-		}
-
-		public void setBeaconInRegion(bool inRegion){
-			beaconInRegion = inRegion;
-		}
-
-		private bool checkBluetooth()
-		{
-			//Validation checks
-			if (!beaconManager.HasBluetooth || !PackageManager.HasSystemFeature(PackageManager.FeatureBluetoothLe))
-			{
-				return false;
-			}
-			else if (!beaconManager.IsBluetoothEnabled)
-			{
-				//bluetooth is not enabled
-				beaconContextualText.Text = "You must enable Bluetooth";
-				return false;
-			}
-			else if (!beaconManager.CheckPermissionsAndService())
-			{
-				beaconContextualText.Text = "There is an issues with bluetooth permission, make sure exposeum is authorized to use the Bluetooth.";
-				return false;
-			}
-			else
-			{
-				return true;
-			}
 		}
 
 	}
---
+++ Exposeum/Exposeum/Model/BeaconFinder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using EstimoteSdk;
+using Android.Util;
+using Android.OS;
+using Android.Content.PM;
+
+namespace Exposeum
+{
+	public class BeaconFinder: BeaconManager.IServiceReadyCallback
+	{
+
+		private BeaconManager beaconManager;
+		private readonly EstimoteSdk.Region region;
+		private bool beaconInRegion;
+		private bool isRanging;
+		private const string TAG = "BeaconFinder";
+
+		public BeaconFinder ()
+		{
+			region = new EstimoteSdk.Region("rid", "B9407F30-F5F8-466E-AFF9-25556B57FE6D");
+			beaconManager = new BeaconManager (this);
+
+			//add event handlers
+			beaconManager.Ranging += BeaconManagerRanging;
+			beaconManager.EnteredRegion += (sender, args) => setBeaconInRegion(true);
+			beaconManager.ExitedRegion += (sender, args) => setBeaconInRegion(false);
+		}
+
+		private void BeaconManagerRanging(object sender, BeaconManager.RangingEventArgs e)
+		{
+			if (e.Beacons == null)
+			{
+				return;
+			}
+
+			Log.Debug("BeaconActivity", "Found {0} beacons.", e.Beacons.Count);
+
+			/*bool close = false;
+			foreach (var beacon in e.Beacons)
+			{
+				var proximity = Utils.ComputeProximity(beacon);
+
+				if (proximity != Utils.Proximity.Unknown)
+					close = true;
+
+				if (proximity != Utils.Proximity.Immediate)
+					continue;
+
+				var accuracy = Utils.ComputeAccuracy(beacon);
+				if (accuracy > .06)
+					continue;
+			}*/
+		}
+
+		public void OnServiceReady()
+		{
+			if (region == null)
+			{
+				throw new Exception("beacon reagion is null");
+			}
+			try
+			{
+				beaconManager.StartRanging(region);
+				Log.Debug(TAG, "Looking for beacons in the region.");
+				isRanging = true;
+
+			}
+			catch (RemoteException e)
+			{
+				isRanging = false;
+				Log.Error(TAG, "Cannot start ranging, {0}", e);
+			}
+
+		}
+
+		public void FindBeacons(){
+
+			if(checkBluetooth())
+				beaconManager.Connect(this);
+		}
+
+		private bool checkBluetooth()
+		{
+			//Validation checks
+			if (!beaconManager.HasBluetooth || !PackageManager.HasSystemFeature(PackageManager.FeatureBluetoothLe))
+			{
+				return false;
+			}
+			else if (!beaconManager.IsBluetoothEnabled)
+			{
+				return false;
+			}
+			else if (!beaconManager.CheckPermissionsAndService())
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		private void setBeaconInRegion(bool inRegion){
+			beaconInRegion = inRegion;
+		}
+
+	}
+}
+
