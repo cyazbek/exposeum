87990921248f7f865954eace6dad30ce8beb560c
Olivier Brochu Dufour <olibd@hotmail.com>
2016-02-06 12:32:24 -0500

    added empty test methods

--- Exposeum/Exposeum.UITests/BeaconTests.cs
+++ Exposeum/Exposeum.UITests/BeaconTests.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+using EstimoteSdk;
 
 namespace Exposeum.UITests
 {
-	public class BeaconTests
+	[TestFixture]
+	public class BeaconTests : IBeaconFinderObserver
 	{
 		public BeaconTests ()
 		{
+		}
+
+		AndroidApp app;
+		BeaconFinder beaconFinder;
+		BeaconManager beaconManager;
+
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			app = ConfigureApp.Android.StartApp ();
+		}
+
+		[Test]
+		public void testBeaconsAreProperlyOrderedByAccuracy ()
+		{
+	
+		}
+
+		public void testNoTwoBeaconsWithSameAccuracyExists(){
+		}
+
+		public void testAllImmediateBeaconInCollection(){
+			
 		}
 	}
 }
