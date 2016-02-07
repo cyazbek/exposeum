a48bf0ad8c7079ac653b103ddc9b8bff5f411978
fkrayem <fkrayem@gmail.com>
2016-02-07 00:33:27 -0500

    Fixed Storyline class. Created methods to find and retrieve Poi from storyline given a beacon. Added poi's to a storyline and tested.

--- Exposeum/Exposeum/Models/Beacon.cs
+++ Exposeum/Exposeum/Models/Beacon.cs
@@ -1,4 +1,5 @@
 using Java.Util;
+using EstimoteSdk;
 
 namespace Exposeum.Models
 {
@@ -22,9 +23,9 @@ namespace Exposeum.Models
             this.major = major;
             this.minor = minor; 
         }
-        public bool compareBeacon(Beacon beacon)
+        public bool compareBeacon(EstimoteSdk.Beacon beacon)
         {
-            if (this.uuid == beacon.uuid && this.minor == beacon.minor && this.major == beacon.major)
+            if (this.uuid.Equals(beacon.ProximityUUID) && this.minor == beacon.Minor && this.major == beacon.Major)
                 return true;
             else
                 return false; 
--- Exposeum/Exposeum/Models/StoryLine.cs
+++ Exposeum/Exposeum/Models/StoryLine.cs
@@ -4,81 +4,74 @@ using SQLite;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using EstimoteSdk;
 
 namespace Exposeum.Models
 {
     public class StoryLine
     {
-        [PrimaryKey, AutoIncrement]
-
-        public int ID { get; set; }
-        /*A list containing all POI's relative to storyline*/
-        public List<POI> POIS { get; set; }
-        /*constructor for the StoryLine*/
+        private string name_en
+         {
+            get; set;
+        }
+        private string name_fr
+         {
+            get; set;
+        }
+        private string audience_en
+         {
+            get; set;
+        }
+        private string audience_fr
+         {
+            get; set;
+        }
+        private string desc_en
+         {
+            get; set;
+        }
+        private string desc_fr 
+         {
+            get; set;
+        }
+        private string duration
+        {
+            get; set;
+        }
+        private List<POI> poiList
+        {
+            get; set;
+        }
         public StoryLine()
         {
-           POIS = new List<POI>();
+            this.poiList = new List<POI>();
         }
-        //Method to add a POI to a storyline. It makes sure that the POI is set to not veisited
-        public void addPOI(POI poi)
+        public void addPoi(POI poi)
         {
-            POIS.Add(poi); 
+            poiList.Add(poi);
+        }
+        public POI findPOI(EstimoteSdk.Beacon beacon)
+        {
+            return poiList.Find(x => x.beacon.compareBeacon(beacon));
+        }
+        public bool hasBeacon(EstimoteSdk.Beacon beacon)
+        {
+            foreach(var poi in poiList)
+            {
+                if (poi.beacon.compareBeacon(beacon))
+                    return true;
+            }
+            return false;
+        }
+        public int getSize()
+        {
+            return poiList.Count; 
         }
 
-        //returns a poi given an ID
-        public POI getPOI(int id)
-        {
-           return this.POIS.Find(x => x.ID == id);  
-        }
-        //get a POI given a beaconID
-        /*public POI getBeaconPOI(UUID id)
-        {
-            return this.POIS.Find(x => x.beaconId == id);
-        }*/
-        //get a POI given a QRcode id
-        /*public POI getQRPOI(int id)
-        {
-            return this.POIS.Find(x => x.QrCodeId == id);
-        }
-        */
         
-        //Changed the status of a POI to visited. 
-        public void visitPoi(POI poi)
-        {
-            poi.visited = true; 
-        }
 
-        public string name_en { get; set; }
-        public string name_fr { get; set; }
-        public string getName()
-        {
-            string lang = Thread.CurrentThread.CurrentCulture.Name;
-            string storyName;
-            if (lang.Contains("fr"))
-            {
-                storyName = this.name_fr;
-            }
-            else storyName = this.name_en;
-            return storyName; 
-        }
-        public string preview_en { get; set; }
 
-        public string preview_fr { get; set; }
 
-        public string getPreview()
-        {
-            string lang = Thread.CurrentThread.CurrentCulture.Name;
-            string storyPreview;
-            if (lang.Contains("fr"))
-            {
-                storyPreview = this.preview_fr;
-            }
-            else storyPreview = this.preview_fr;
-            return storyPreview;
-        }
-        public string audience_en { get; set; }
-
-        public string audience_fr { get; set; }
         public string getAudience()
         {
             string lang = Thread.CurrentThread.CurrentCulture.Name;
@@ -90,7 +83,6 @@ namespace Exposeum.Models
             else storyAudience = this.audience_en;
             return storyAudience;
         }
-        public int duration { get; set; }
         
     }
 }
