5ed80d1153c0f297fd645e2a4076551375a9d9b6
Olivier Brochu Dufour <olibd@hotmail.com>
2016-02-07 00:14:45 -0500

    Added a new unit test project dedicate to unit tests not related to UI. This is especially necessary since it is impossible to add the EstimoteSDK to the Exposeum.UITests project due to incompatibilities. This UnitTests project has the proper configurations to run normal Android Tests.
    The structure of this project should go as follows:
    
    /USERSTORYIDENTIFIER/UnitTestForTestableClass
    
    a.k.a One folder per user story, one test class per testable class in the main project

---
+++ Exposeum/UnitTests/US13/BeaconFinderTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using EstimoteSdk;
+using Android.App;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class BeaconFinderTests
+	{
+		[SetUp]
+		public void Setup ()
+		{
+			
+			BeaconManager beaconManager = new BeaconManager(Application.Context);
+		}
+
+
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("test1");
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+}
+
