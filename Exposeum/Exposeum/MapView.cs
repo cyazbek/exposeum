fcb0f111b648fc1579e9e0eee69b925dc97f7908
Maxwell Mowbray <mmowbray@mentlegen.com>
2016-02-11 17:56:05 -0500

    Added floor placeholders and fake points to draw POIs on different floors

--- Exposeum/Exposeum/MapView.cs
+++ Exposeum/Exposeum/MapView.cs
@@ -18,7 +18,6 @@ namespace Exposeum
 	public class MapView : View
 	{
 		private static readonly int InvalidPointerId = -1;
-		private Drawable _map;
 		private readonly ScaleGestureDetector _scaleDetector;
 		private int _activePointerId = InvalidPointerId;
 		private float _lastTouchX;
@@ -27,41 +26,56 @@ namespace Exposeum
 		private float _posY;
 		private float _scaleFactor = 1.0f;
 		private PointOfInterest _lastClickedPOI;
-
+		private Floor _currentFloor;
 		private Context _context;
 
-		//test points to be drawn on map
 		private List<PointOfInterest> samplePoints = new List<PointOfInterest>();
 
-		//test edges to be draw on map
-		private List<Models.Edge> sampleEdges = new List<Models.Edge>();
+		//test points to be drawn on map
+		private List<Floor> sampleFloors = new List<Floor>();
 
 		public MapView (Context context) : base(context, null, 0)
 		{
 			_context = context;
 
-			_map = context.Resources.GetDrawable (Resource.Drawable.floor_5);
-			_map.SetBounds (0, 0, _map.IntrinsicWidth, _map.IntrinsicHeight);
 			_scaleDetector = new ScaleGestureDetector (context, new MyScaleListener (this));
 
-			//push some sample points to draw on our map
-			samplePoints.Add(new PointOfInterest(0.53f, 0.46f, "site 1"));
-			samplePoints.Add(new PointOfInterest(0.62f, 0.64f, "site 2"));
-			samplePoints.Add(new PointOfInterest(0.60f, 0.82f, "site 3"));
-			samplePoints.Add(new PointOfInterest(0.85f, 0.88f, "site 4"));
-			samplePoints.Add(new PointOfInterest(0.925f, 0.55f, "site 5"));
-			samplePoints.Add(new PointOfInterest(0.77f, 0.265f, "site 6"));
-			samplePoints.Add(new PointOfInterest(0.56f, 0.19f, "site 7"));
-			samplePoints.Add(new PointOfInterest(0.346f, 0.886f, "site 8"));
-			samplePoints.Add(new PointOfInterest(0.241f, 0.266f, "site 9"));
+			Floor floor1 = new Floor (Resources.GetDrawable (Resource.Drawable.floor_1));
+			Floor floor2 = new Floor (Resources.GetDrawable (Resource.Drawable.floor_2));
+			Floor floor3 = new Floor (Resources.GetDrawable (Resource.Drawable.floor_3));
+			Floor floor4 = new Floor (Resources.GetDrawable (Resource.Drawable.floor_4));
+			Floor floor5 = new Floor (Resources.GetDrawable (Resource.Drawable.floor_5));
+
+			floor1.addPointOfInterest (new PointOfInterest (0.53f, 0.46f, "site 1"));
+			floor1.addPointOfInterest (new PointOfInterest (0.60f, 0.82f, "site 1"));
+
+			floor2.addPointOfInterest (new PointOfInterest (0.90f, 0.46f, "site 1"));
+			floor2.addPointOfInterest (new PointOfInterest (0.53f, 0.66f, "site 1"));
+
+			floor3.addPointOfInterest (new PointOfInterest (0.53f, 0.43f, "site 1"));
+			floor3.addPointOfInterest (new PointOfInterest (0.77f, 0.46f, "site 1"));
+
+			floor4.addPointOfInterest (new PointOfInterest (0.53f, 0.46f, "site 1"));
+			floor4.addPointOfInterest (new PointOfInterest (0.73f, 0.16f, "site 1"));
+
+			floor5.addPointOfInterest (new PointOfInterest (0.241f, 0.46f, "site 1"));
+			floor5.addPointOfInterest (new PointOfInterest (0.53f, 0.88f, "site 1"));
+
+			sampleFloors.Add (floor1);
+			sampleFloors.Add (floor2);
+			sampleFloors.Add (floor3);
+			sampleFloors.Add (floor4);
+			sampleFloors.Add (floor5);
+
+			_currentFloor = sampleFloors [0];
 		}
 			
 		public void OnMapSliderProgressChange (object sender, SeekBar.ProgressChangedEventArgs e)
 		{
-			_map = _context.Resources.GetDrawable (Resource.Drawable.planmetro2013);
-			_map.SetBounds (0, 0, _map.IntrinsicWidth, _map.IntrinsicHeight);
-			sampleEdges.Clear ();
-			samplePoints.Clear ();
+			_currentFloor = sampleFloors [e.Progress];
+			_lastClickedPOI = null;
+
+			this.Invalidate ();
 		}
 
 		public override bool OnTouchEvent (MotionEvent ev)
@@ -78,7 +92,7 @@ namespace Exposeum
 					if (_lastClickedPOI == null) {
 						_lastClickedPOI = selected;
 					} else if (_lastClickedPOI != selected) {
-						sampleEdges.Add (new Models.Edge (_lastClickedPOI, selected));
+						_currentFloor.addEdge(new Models.Edge (_lastClickedPOI, selected));
 						_lastClickedPOI = selected;
 					}
 				}
@@ -134,18 +148,9 @@ namespace Exposeum
 		{
 			base.OnDraw (canvas);
 			canvas.Save ();
-			canvas.Translate (_posX + _scaleFactor * -_map.IntrinsicWidth / 2, _posY + _scaleFactor * -_map.IntrinsicHeight / 2);
+			canvas.Translate (_posX + _scaleFactor * -_currentFloor.Image.IntrinsicWidth / 2, _posY + _scaleFactor * -_currentFloor.Image.IntrinsicHeight / 2);
 			canvas.Scale (_scaleFactor, _scaleFactor);
-			_map.Draw (canvas);
-
-			//draw edges on top of map
-			foreach (Models.Edge edge in sampleEdges)
-				edge.Draw (canvas, _map.IntrinsicWidth, _map.IntrinsicHeight);
-			
-			//draw pins on top of map
-			foreach (PointOfInterest poi in samplePoints)
-				poi.Draw (canvas, _map.IntrinsicWidth, _map.IntrinsicHeight);
-
+			_currentFloor.Draw (canvas);
 			canvas.Restore ();
 		}
 			
@@ -180,9 +185,9 @@ namespace Exposeum
 
 			PointOfInterest clicked = null;
 
-			foreach (PointOfInterest poi in samplePoints) {
-				float poiX = _posX + (_scaleFactor * _map.IntrinsicWidth * poi.U) - ((_scaleFactor * _map.IntrinsicWidth) / 2);
-				float poiY = _posY + (_scaleFactor * _map.IntrinsicHeight * poi.V) - ((_scaleFactor * _map.IntrinsicHeight) / 2);
+			foreach (PointOfInterest poi in _currentFloor.PointsOfInterest) {
+				float poiX = _posX + (_scaleFactor * _currentFloor.Image.IntrinsicWidth * poi.U) - ((_scaleFactor * _currentFloor.Image.IntrinsicWidth) / 2);
+				float poiY = _posY + (_scaleFactor * _currentFloor.Image.IntrinsicHeight * poi.V) - ((_scaleFactor * _currentFloor.Image.IntrinsicHeight) / 2);
 
 				if (Math.Sqrt (Math.Pow (screenX - poiX, 2) + Math.Pow (screenY - poiY, 2)) <= poi.Radius * _scaleFactor) {
 					clicked = poi;
---
+++ Exposeum/Exposeum/Models/Floor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.Graphics.Drawables;
+using Android.Graphics;
+
+namespace Exposeum.Models
+{
+	public class Floor
+	{
+		private Drawable _floorPlan;
+		private List<Edge> _floorEdges = new List<Edge>();
+		private List<PointOfInterest> _floorPointsOfInterest = new List<PointOfInterest>();
+
+		public Floor (Drawable floorPlan)
+		{
+			_floorPlan = floorPlan;
+			_floorPlan.SetBounds (0, 0, _floorPlan.IntrinsicWidth, _floorPlan.IntrinsicHeight);
+		}
+
+		public void Draw(Canvas canvas)
+		{
+			_floorPlan.Draw (canvas);
+
+			//draw edges on top of map
+			foreach (Models.Edge edge in _floorEdges)
+				edge.Draw (canvas, _floorPlan.IntrinsicWidth, _floorPlan.IntrinsicHeight);
+
+			//draw pins on top of edges
+			foreach (PointOfInterest poi in _floorPointsOfInterest)
+				poi.Draw (canvas, _floorPlan.IntrinsicWidth, _floorPlan.IntrinsicHeight);
+		}
+
+		public void addEdge(Edge e)
+		{
+			_floorEdges.Add (e);
+		}
+
+		public void addPointOfInterest(PointOfInterest e)
+		{
+			_floorPointsOfInterest.Add (e);
+		}
+
+		public Drawable Image
+		{
+			get { return this._floorPlan; }
+		}
+
+		public List<PointOfInterest> PointsOfInterest
+		{
+			get { return this._floorPointsOfInterest; }
+		}
+
+	}
+}
