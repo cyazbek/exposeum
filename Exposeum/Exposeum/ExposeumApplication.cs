69478e0b83c575dbf7459c2a32a4dffed0c0b6c7
Olivier Brochu Dufour <olibd@hotmail.com>
2016-02-19 15:44:14 -0500

    Extended the Android application class. The idea is to have it create a singleton of the beaconfinder

---
+++ Exposeum/Exposeum/ExposeumApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+
+namespace Exposeum
+{
+	public class ExposeumApplication: Application
+	{
+		public ExposeumApplication ()
+		{
+		}
+
+		public override void OnCreate(){
+			initSigletons ();
+		}
+
+		protected void initSigletons(){
+			
+		}
+	}
+}
+
