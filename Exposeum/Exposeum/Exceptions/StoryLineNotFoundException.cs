2fb7b32ca37238851199b9d60bed77cfed3a398c
Olivier Brochu Dufour <olibd@hotmail.com>
2016-02-19 15:58:41 -0500

    Throwing StoryLineNotFoundException if trying to range without a set storyline

---
+++ Exposeum/Exposeum/Exceptions/StoryLineNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exposeum
+{
+	public class StoryLineNotFoundException: Exception
+	{
+		public StoryLineNotFoundException (string message) : base(message)
+		{
+		}
+	}
+}
+
