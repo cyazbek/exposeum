06c7bb15052cad4b2875f6892c403266af3906a7
Maxwell Mowbray <mmowbray@mentlegen.com>
2016-02-26 14:02:57 -0500

    Adapted Map MVC structure to use MapElement's Floor attribute, removed Floor's MapElementList attributes, as per class diagram

--- Exposeum/Exposeum/Controllers/StorylineController.cs
+++ Exposeum/Exposeum/Controllers/StorylineController.cs
@@ -7,13 +7,8 @@ namespace Exposeum.Controllers
         public StoryLine CurrentStoryLine { get; set; }
 
         public void CreateTempStoryline ()
-        {
-            //how it should be when create/parsing the JSON file and setting the data up.
-            Floor floor1 = new Floor(Android.App.Application.Context.Resources.GetDrawable(Resource.Drawable.floor_1));
-            PointOfInterest poi1 = new PointOfInterest(0.53f, 0.46f, floor1);
-            PointOfInterestDescription description1 = new PointOfInterestDescription("The First :: Title"
-                    ,"A Summary about the first :: summary","A Full Description about the first :: Description");
-            CurrentStoryLine.addPoi(poi1);
+		{
+			//currently, the dummy seed data for the current storyline is in Map.cs seedData() method.
         }
     }
 }
