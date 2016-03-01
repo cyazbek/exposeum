6be7856deb7642edf2b8750e5e1aa79c1a226599
Andrea <andrea18fp@hotmail.com>
2016-03-01 16:21:18 -0500

    EX 105 #In Progress - Created Top Menu Controller

---
+++ Exposeum/Exposeum/Controllers/TopMenuController.cs
@@ -0,0 +1,12 @@
+using Android.Views;
+using Exposeum.Models;
+
+namespace Exposeum.Controllers
+{
+    public class TopMenuController
+    {
+        private BeaconFinder _beaconFinder = BeaconFinder.getInstance();
+
+
+    }
+}
