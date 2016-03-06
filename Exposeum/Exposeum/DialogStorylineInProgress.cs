54ef76d378f230cfd40ce96a38e765f975f2420e
Andrea <andrea18fp@hotmail.com>
2016-03-05 23:50:37 -0500

    Modified storyline in progress popup

--- Exposeum/Exposeum/DialogStorylineInProgress.cs
+++ Exposeum/Exposeum/DialogStorylineInProgress.cs
@@ -21,43 +21,50 @@ namespace Exposeum
         {
             this.storyLine = storyLine;
         }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
+
             var view = inflater.Inflate(Resource.Layout.StoryLine_dialog_inProgress, container, false);
-            view.FindViewById<ImageView>(Resource.Id.sotryLineDialogPic).SetImageResource(storyLine.ImageId);
-            view.FindViewById<TextView>(Resource.Id.storyLineDialogTitle).Text = storyLine.getName();
-            view.FindViewById<TextView>(Resource.Id.storyLineDialogAudience).Text = storyLine.getAudience();
-            view.FindViewById<TextView>(Resource.Id.storyLineDialogDuration).Text = storyLine.duration + " min";
-            view.FindViewById<TextView>(Resource.Id.storyLineDialogDescription).Text = storyLine.getDescription();
+                view.FindViewById<ImageView>(Resource.Id.sotryLineDialogPic).SetImageResource(storyLine.ImageId);
+                view.FindViewById<TextView>(Resource.Id.storyLineDialogTitle).Text = storyLine.getName();
+                view.FindViewById<TextView>(Resource.Id.storyLineDialogAudience).Text = storyLine.getAudience();
+                view.FindViewById<TextView>(Resource.Id.storyLineDialogDuration).Text = storyLine.duration + " min";
+
             var textview = view.FindViewById<TextView>(Resource.Id.storyLineDialogDescription);
-            textview.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
-            textview.VerticalScrollBarEnabled = true;
-            textview.HorizontalFadingEdgeEnabled = true;
+                textview.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
+                textview.VerticalScrollBarEnabled = true;
+                textview.HorizontalFadingEdgeEnabled = true;
+
             var buttonToResume = view.FindViewById<Button>(Resource.Id.storyLineDialogButtonToResume);
             var buttonToReset = view.FindViewById<Button>(Resource.Id.storyLineDialogButtonToReset);
+                
+
             if (Language.getLanguage() == "fr")
             {
+                view.FindViewById<TextView>(Resource.Id.storyInProgress).Text = "Actuallement en cours.";
                 buttonToResume.Text = "Continuer";
                 buttonToReset.Text = "Recommencer";
-            }
-                
+            }  
             else
             {
-                buttonToResume.Text = "Resume Journey";
-                buttonToReset.Text = "Restart Jouney";
+                view.FindViewById<TextView>(Resource.Id.storyInProgress).Text = "Storyline already in progress.";
+                buttonToResume.Text = "Resume";
+                buttonToReset.Text = "Restart";
             }
+
             buttonToResume.Click += delegate {
                
             };
+
             buttonToReset.Click += delegate {
 
             };
+
             this.Dialog.SetCanceledOnTouchOutside(true);
             return view;
         }
-
-        
 
         public override void OnActivityCreated(Bundle savedInstanceState)
         {
