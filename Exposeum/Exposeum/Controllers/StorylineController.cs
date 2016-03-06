6d065c7fc02ee1616851e6f89b1fb50fb9b03051
Olivier Brochu Dufour <olibd@hotmail.com>
2016-03-05 20:04:20 -0500

    Added SelectStoryLine(int storylinePosition), ShowSelectedStoryLineDialog(FragmentTransaction transaction), SetActiveStoryLine() to the controller

--- Exposeum/Exposeum/Controllers/StorylineController.cs
+++ Exposeum/Exposeum/Controllers/StorylineController.cs
@@ -7,11 +7,34 @@ namespace Exposeum.Controllers
     {
 
 		private static Map map = MapService.GetMapInstance();
+		private static StoryLine selectedStoryLine;
 
-		public StoryLineListAdapter GetStoryLines(Activity activity){
+		public static StoryLineListAdapter GetStoryLines(Activity activity){
 			return new StoryLineListAdapter(activity, map.getStoryLineList);
 		}
 
+		public static void SelectStoryLine(int storylinePosition){
+			selectedStoryLine = map.getStoryLineList[storylinePosition];
+		}
 
+		public static void ShowSelectedStoryLineDialog(FragmentTransaction transaction){
+			
+				
+				DialogFragment dialog;
+				if(selectedStoryLine.currentStatus==Status.inProgress)
+				{
+					dialog = new DialogStorylineInProgress(selectedStoryLine);
+				}
+				else
+				{
+					dialog = new DialogStoryline(selectedStoryLine);
+				}
+
+				dialog.Show(transaction, "Story Line title");
+		}
+
+		public static void SetActiveStoryLine(){
+			map.CurrentStoryline = selectedStoryLine;
+		}
     }
 }
