97520ad945e6a37624c5fe257dbb586eb0ed558f
fkrayem <fkrayem@gmail.com>
2016-03-13 01:25:57 -0500

    EX-133 #InProgress Added Tables for Edges, StoryLineDescriptionList

--- Exposeum/Exposeum/Tables/StorylineDescriptionList.cs
+++ Exposeum/Exposeum/Tables/StorylineDescriptionList.cs
@@ -1,18 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using SQLite;
 
 namespace Exposeum.Tables
 {
+    
     class StorylineDescriptionList
     {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int ID { get; set; }
+        public int storyLineId { get; set; }
+        public int descriptionId { get; set; }
+
     }
 }
