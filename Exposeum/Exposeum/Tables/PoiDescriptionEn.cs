40aa9443a15a202b4d4661979529444d7cb499e8
Andrea <andrea18fp@hotmail.com>
2016-03-13 01:26:46 -0500

    EX-133 #InProgress created tables: Beacon, PoiDescriptionEn, PoiDescriptionFr

--- Exposeum/Exposeum/Tables/PoiDescriptionEn.cs
+++ Exposeum/Exposeum/Tables/PoiDescriptionEn.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using SQLite;
 
 namespace Exposeum.Tables
 {
+    [Table("Items")]
     class PoiDescriptionEn
     {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int ID { get; set; }
+        public string title { get; set; }
+        public string sumary { get; set; }
     }
 }
