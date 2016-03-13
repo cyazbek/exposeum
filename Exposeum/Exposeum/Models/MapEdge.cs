023fad8c73936a53dd33cee602bdf775dbacde7a
malikr <fatindian@gmail.com>
2016-03-12 21:27:35 -0500

    Added ShortestPath Service to computer shortest path between two nodes

--- Exposeum/Exposeum/Models/MapEdge.cs
+++ Exposeum/Exposeum/Models/MapEdge.cs
@@ -6,13 +6,13 @@ namespace Exposeum.Models
 {
 	public class MapEdge : IEdge<MapElement>
 	{
-		private double Distance {get; set;}
+		public double Distance {get; set;}
 
         public MapEdge(MapElement start, MapElement end)
         {
             this.Source = start;
             this.Target = end;
-            this.Distance = 0;
+            this.Distance = 1;
         }
 
         public MapEdge (MapElement start, MapElement end, double distance)
