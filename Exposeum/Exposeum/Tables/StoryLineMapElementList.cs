7283f90fac48b4704f6c2474c8cbc931eff170f8
fkrayem <fkrayem@gmail.com>
2016-03-13 01:14:31 -0500

    EX-133 #InProgress Created Tables: StoryLine, StoryLineMapElementList

--- Exposeum/Exposeum/Tables/StoryLineMapElementList.cs
+++ Exposeum/Exposeum/Tables/StoryLineMapElementList.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using SQLite; 
 
 namespace Exposeum.Tables
 {
-    class StoryLineMapElementList
+    public class StoryLineMapElementList
     {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int ID { get; set; }
+        public int storyLineId { get; set; }
+        public int mapElementId { get; set; }
+
     }
 }
