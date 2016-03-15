d9dbc5a407d8104cf35410be449440681c9e477c
Chris <christopher.yazbek@gmail.com>
2016-03-15 02:19:01 -0400

    Preping the class required for doing the scanner Archet4ecture is missing to how we want to implement this.

---
+++ Exposeum/Exposeum/QRScanActivity.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing.Mobile;
+
+namespace Exposeum
+{
+    [Activity(Label = "ZXing.Net.Mobile", MainLauncher = true, Theme = "@android:style/Theme.Holo.Light", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden)]
+    public class QRScanActivity : Activity
+    {
+        Button buttonScanCustomView;
+        Button buttonScanDefaultView;
+        Button buttonContinuousScan;
+        Button buttonFragmentScanner;
+        Button buttonGenerate;
+
+        MobileBarcodeScanner scanner;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Initialize the scanner first so we can track the current context
+            MobileBarcodeScanner.Initialize(Application);
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+
+            //Create a new instance of our Scanner
+            scanner = new MobileBarcodeScanner();
+
+            buttonScanDefaultView = this.FindViewById<Button>(Resource.Id.buttonScanDefaultView);
+            buttonScanDefaultView.Click += async delegate {
+
+                //Tell our scanner to use the default overlay
+                scanner.UseCustomOverlay = false;
+
+                //We can customize the top and bottom text of the default overlay
+                scanner.TopText = "Hold the camera up to the barcode\nAbout 6 inches away";
+                scanner.BottomText = "Wait for the barcode to automatically scan!";
+
+                //Start scanning
+                var result = await scanner.Scan();
+
+                HandleScanResult(result);
+            };
+        }
+
+        void HandleScanResult(ZXing.Result result)
+        {
+            string msg = "";
+
+            if (result != null && !string.IsNullOrEmpty(result.Text))
+                msg = "Found Barcode: " + result.Text;
+            else
+                msg = "Scanning Canceled!";
+
+            this.RunOnUiThread(() => Toast.MakeText(this, msg, ToastLength.Short).Show());
+        }
+    }
+}
