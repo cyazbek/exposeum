72101de5a7bda32fa35a31c1422e3a3c04f6e110
Olivier Brochu Dufour <olibd@hotmail.com>
2016-03-16 10:53:00 -0400

    EX-124 Implemented the IPath interface

--- Exposeum/Exposeum/Models/ShortPath.cs
+++ Exposeum/Exposeum/Models/ShortPath.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using Exposeum.Models;
+using System.Collections.Generic;
 
 namespace Exposeum
 {
-	public class ShortPath
+	public class ShortPath: IPath
 	{
+		public Status CurrentStatus { get; set; }
+		public List<MapElement> MapElements { get; set; }
+
 		public ShortPath ()
 		{
+		}
+
+		public void AddMapElement (MapElement e){
+			
+		}
+
+		public void UpdateProgress (MapElement mapElement){
+			
+		}
+
+		public PointOfInterest FindPoi (EstimoteSdk.Beacon beacon){
+			
+		}
+
+		public bool HasBeacon (EstimoteSdk.Beacon beacon){
+			
 		}
 	}
 }
