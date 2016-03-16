d0498811fbf6bc5e68f6499c16e6c138eaf8856b
Olivier Brochu Dufour <olibd@hotmail.com>
2016-03-16 10:52:35 -0400

    added methods to the IPath interface

--- Exposeum/Exposeum/Models/IPath.cs
+++ Exposeum/Exposeum/Models/IPath.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Exposeum.Models;
+using System.Collections.Generic;
 
 namespace Exposeum
 {
 	public interface IPath
 	{
+		Status CurrentStatus { get; set; }
+		List<MapElement> MapElements { get; set; }
+
+		void AddMapElement (MapElement e);
+		void UpdateProgress (MapElement mapElement);
+		PointOfInterest FindPoi (EstimoteSdk.Beacon beacon);
+		bool HasBeacon (EstimoteSdk.Beacon beacon);
 	}
 }
 
