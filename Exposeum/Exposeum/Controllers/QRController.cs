0c46f572ea1411440378cac6f495a1a3a78f409a
Maxwell Mowbray <mmowbray@mentlegen.com>
2016-03-16 16:32:01 -0400

    Made QRController a singleton

--- Exposeum/Exposeum/Controllers/QRController.cs
+++ Exposeum/Exposeum/Controllers/QRController.cs
@@ -4,6 +4,14 @@ namespace Exposeum.Controllers
 {
 	public class QRController
 	{
+		private QRController _instance;
+
+		public static QRController GetInstance(){
+			if (_instance == null)
+				_instance = new QRController ();
+			return _instance;
+		}
+
 		public QRController ()
 		{
 		}
