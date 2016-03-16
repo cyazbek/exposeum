acdc4bcb368677faa85144e23779e4a71aed1760
Olivier Brochu Dufour <olibd@hotmail.com>
2016-03-16 12:58:09 -0400

    EX-124 Implemented the Deep cloning service

--- Exposeum/Exposeum/Services/DeepCloneService.cs
+++ Exposeum/Exposeum/Services/DeepCloneService.cs
@@ -2,8 +2,16 @@
 
 namespace Exposeum
 {
-	public interface IDeepClone
+	public abstract class DeepCloneService
 	{
+		/// <summary>
+		/// Perform a deep Copy of the object, using Json as a serialisation method.
+		/// </summary>
+		/// <typeparam name="T">The type of object being copied.</typeparam>
+		/// <param name="source">The object instance to copy.</param>
+		/// <returns>The copied object.</returns>
+		public static T Clone<T>(this T source);
+
 	}
 }
 
