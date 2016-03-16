4d62685cf63743cafa8cc176a9c0c46732c4a426
Andrea <andrea18fp@hotmail.com>
2016-03-16 01:17:18 -0400

    EX-135 #InProgress started implementation of mappers: EdgeMapper and MapElementsMapper

---
+++ Exposeum/Exposeum/Mappers/EdgeMapper.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Exposeum.Models;
+using Exposeum.TDGs;
+
+namespace Exposeum.Mappers
+{
+    public class EdgeMapper
+    {
+        private readonly EdgeTDG _edgeTdg = new EdgeTDG();
+        private readonly List<Edge> _listOfEdges = new List<Edge>();
+        private readonly MapElementsMapper _mapElementsMapper = new MapElementsMapper();
+
+
+        public List<Edge> GetAllEdges()
+        {
+            List<Tables.Edge> listEdges = _edgeTdg.GetAllEdges();
+
+            foreach (var edge in listEdges)
+            {
+                int startElement = edge.startMapElementId;
+                int endElement = edge.endMapElementId;
+                double distance = edge.distance;
+
+                MapElement startMapElement = _mapElementsMapper.GetMapElement(startElement);
+                MapElement endMapElement = _mapElementsMapper.GetMapElement(endElement);
+                
+                Edge edgeModel = new Edge(startMapElement, endMapElement, distance);
+
+                _listOfEdges.Add(edgeModel);
+            }
+
+            return _listOfEdges;
+        }
+    }
+}
---
+++ Exposeum/Exposeum/Mappers/MapElementsMapper.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Exposeum.Models;
+using Exposeum.Tables;
+using Exposeum.TDGs;
+
+
+namespace Exposeum.Mappers
+{
+    public class MapElementsMapper
+    {
+        private readonly MapElementsTDG _mapElementsTdg = new MapElementsTDG();
+        private readonly List<MapElement> _listOfMapElements = new List<MapElement>();
+
+        public MapElement GetMapElement(int id)
+        {
+            MapElements mapElementTable = _mapElementsTdg.GetMapElement(id);
+
+            string discrinator = mapElementTable.discriminator;
+
+            switch (discrinator)
+            {
+                case "PointOfInterest":
+                    PointOfInterest pointOfInterest = null;
+                    return pointOfInterest;
+                case "WayPoint":
+                    PointOfTravel pointOfTravel = null;
+                    return pointOfTravel;
+                default:
+                    return null;
+            }
+        }
+
+        public List<MapElement> GetAllMapElements()
+        {
+            List<Tables.MapElements> listMapElementsTable = _mapElementsTdg.GetAllMapElements();
+
+            foreach (var mapElementTable in listMapElementsTable)
+            {
+                int idMapElementTable = mapElementTable.ID;
+
+                MapElement mapElementModel = GetMapElement(idMapElementTable);
+
+                _listOfMapElements.Add(mapElementModel);
+            }
+
+            return _listOfMapElements;
+        }
+    }
+}
