5e1b430b42d47ad6cc8c49c1b6221af604ae5d21
fkrayem <fkrayem@gmail.com>
2016-03-18 00:23:17 -0400

    Storyline Attributes added.

--- Exposeum/Exposeum/TempModels/Storyline.cs
+++ Exposeum/Exposeum/TempModels/Storyline.cs
@@ -1,8 +1,23 @@
 
+using System.Collections.Generic;
+
 namespace Exposeum.TempModels
 {
     public class Storyline
     {
+        public int _storylineId { get; set; }
+        public int _imageId { get; set; }
+        public int _duration { get; set; }
+        public int _floorsCovered { get; set; }
+        public List<MapElement> _mapElements { get; set; }
+        public string _intendedAudience { get; set;}
+        public MapElement _lastVisitedMapElement { get; set; }
+        public Models.Status _status; 
+        public StorylineDescription _storylineDescription { get; set; }
 
+        public Storyline()
+        {
+            _mapElements = new List<MapElement>();
+        }
     }
 }
