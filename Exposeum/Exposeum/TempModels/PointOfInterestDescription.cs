1b0fb0cee4c926ea93f8774ee1f8b7e4b4144910
fkrayem <fkrayem@gmail.com>
2016-03-18 00:41:56 -0400

    POIDescription added to the tempModel

--- Exposeum/Exposeum/TempModels/PointOfInterestDescription.cs
+++ Exposeum/Exposeum/TempModels/PointOfInterestDescription.cs
@@ -3,5 +3,9 @@ namespace Exposeum.TempModels
 {
     public class PointOfInterestDescription
     {
+        public int _pointOfInterestDescriptionId { get; set; }
+        public string title { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
     }
 }
--- Exposeum/UnitTests/TDGTests/PoiDescriptionFrTDGTest.cs
+++ Exposeum/UnitTests/TDGTests/PoiDescriptionFrTDGTest.cs
@@ -19,6 +19,7 @@ namespace UnitTests
             _setObject.ID = 1;
             _setObject.title = "title";
             _setObject.summary = "summary";
+            _setObject.description = "description";
         }
 
         [Test()]
@@ -42,6 +43,7 @@ namespace UnitTests
             _setObject.ID = 1;
             _setObject.title = "title";
             _setObject.summary = "summary";
+            _setObject.description = "description";
             _objectTDG.Add(_testObject);
             _testObject.title = "title2";
             string title = _testObject.title;
