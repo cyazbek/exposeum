23c4d38471d7b9e1706c6d801e5789a5cd6e2661
fkrayem <fkrayem@gmail.com>
2016-03-18 00:04:00 -0400

    Floor model sttributes added.

--- Exposeum/Exposeum/TempModels/Floor.cs
+++ Exposeum/Exposeum/TempModels/Floor.cs
@@ -1,8 +1,18 @@
 
 
+using Android.Graphics;
+
 namespace Exposeum.TempModels
 {
     public class Floor
     {
+        public int _floorPlan { get; set; } 
+        public Paint _paint;
+
+        public Floor()
+        {
+            this._paint = new Paint(); 
+        }
+
     }
 }
