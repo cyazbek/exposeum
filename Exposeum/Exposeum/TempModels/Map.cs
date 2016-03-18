ebd1eb5e6f86a5efcaa193a5815eacb1c084f0cc
fkrayem <fkrayem@gmail.com>
2016-03-18 00:12:17 -0400

    Added Attributes to Map and made it a singleton

--- Exposeum/Exposeum/TempModels/Map.cs
+++ Exposeum/Exposeum/TempModels/Map.cs
@@ -1,8 +1,28 @@
-
+using System.Collections.Generic;
 
 namespace Exposeum.TempModels
 {
     public class Map
     {
+        private static Map _instance;
+        public List<Edge> _edges { get; set; }
+        public List<Storyline> _storylines { get; set; }
+        public Storyline _currentStoryline { get; set; }
+        public List<Floor> _floors { get; set; }
+        public Floor _currentFloor;  
+
+        private Map()
+        {
+            _edges = new List<Edge>();
+            _storylines = new List<Storyline>();
+            _floors = new List<Floor>(); 
+        }
+        
+        public static Map GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Map();
+            return _instance; 
+        }
     }
 }
