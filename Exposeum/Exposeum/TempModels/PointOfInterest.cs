3647d6251d2ee560979d41a6af619e45f9b6c26d
fkrayem <fkrayem@gmail.com>
2016-03-18 00:32:24 -0400

    Added Waypoint Model and its attribute

--- Exposeum/Exposeum/TempModels/PointOfInterest.cs
+++ Exposeum/Exposeum/TempModels/PointOfInterest.cs
@@ -3,5 +3,14 @@ namespace Exposeum.TempModels
 {
     public class PointOfInterest:MapElement
     {
+        public int _pointOfInterestId { get; set; }
+        public Beacon _beacon {get; set; }      
+        public int _storyLineId { get; set; }
+        public PointOfInterestDescription _description { get; set; }
+        public ExhibitionContent _exhibitionContent { get; set; }
+        public PointOfInterest()
+        {
+            _beacon = new Beacon();  
+        }
     }
 }
