90d9089b2cb6c8170deb47a5031bb9161df42487
Andrea <andrea18fp@hotmail.com>
2016-03-18 23:51:19 -0400

    EX-135 Implemented MapElementMapper

--- Exposeum/Exposeum/TempModels/PointOfInterest.cs
+++ Exposeum/Exposeum/TempModels/PointOfInterest.cs
@@ -3,7 +3,6 @@ namespace Exposeum.TempModels
 {
     public class PointOfInterest:MapElement
     {
-        public int _pointOfInterestId { get; set; }
         public Beacon _beacon {get; set; }      
         public int _storyLineId { get; set; }
         public PointOfInterestDescription _description { get; set; }
--- Exposeum/Exposeum/TempModels/PointOfInterestDescription.cs
+++ Exposeum/Exposeum/TempModels/PointOfInterestDescription.cs
@@ -4,7 +4,7 @@ namespace Exposeum.TempModels
 {
     public class PointOfInterestDescription
     {
-        public int _pointOfInterestDescriptionId { get; set; }
+        public int _id { get; set; }
         public string title { get; set; }
         public string summary { get; set; }
         public string description { get; set; }
