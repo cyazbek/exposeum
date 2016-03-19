859beb05d498ce7886478368afa78d3589fe2f63
malikr <fatindian@gmail.com>
2016-03-19 19:30:41 -0400

    changed edgemapper to take in MapEdge to avoid build errors, removed beaconsummarypopuptests file (was useless)

--- Exposeum/Exposeum/Mappers/EdgeMapper.cs
+++ Exposeum/Exposeum/Mappers/EdgeMapper.cs
@@ -7,11 +7,11 @@ namespace Exposeum.Mappers
     public class EdgeMapper
     {
         private readonly EdgeTDG _edgeTdg = EdgeTDG.GetInstance();
-        private readonly List<Edge> _listOfEdges = new List<Edge>();
+        private readonly List<MapEdge> _listOfEdges = new List<MapEdge>();
         private readonly MapElementsMapper _mapElementsMapper = new MapElementsMapper();
 
 
-        public List<Edge> GetAllEdges()
+        public List<MapEdge> GetAllEdges()
         {
             List<Tables.Edge> listEdges = _edgeTdg.GetAllEdges();
 
@@ -24,7 +24,7 @@ namespace Exposeum.Mappers
                 MapElement startMapElement = _mapElementsMapper.GetMapElement(startElement);
                 MapElement endMapElement = _mapElementsMapper.GetMapElement(endElement);
                 
-                Edge edgeModel = new Edge(startMapElement, endMapElement, distance);
+                MapEdge edgeModel = new MapEdge(startMapElement, endMapElement, distance);
 
                 _listOfEdges.Add(edgeModel);
             }
