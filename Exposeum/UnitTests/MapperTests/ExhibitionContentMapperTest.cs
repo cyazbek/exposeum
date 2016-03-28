9ee74d0bd445042c54cf4703a159dbeaaca68092
Andrea <andrea18fp@hotmail.com>
2016-03-28 03:07:33 -0400

    Implemented ExhibitionContentMapperTest

--- Exposeum/UnitTests/MapperTests/ExhibitionContentMapperTest.cs
+++ Exposeum/UnitTests/MapperTests/ExhibitionContentMapperTest.cs
@@ -18,12 +18,20 @@ namespace UnitTests
     public class ExhibitionContentMapperTest
     {
         private static ExhibitionContentMapper _instance;
-        private AudioContent _audioContent;
-        private VideoContent _videoContent;
-        private TextContent _textContent;
-        private ImageContent _imageContent;
 
-        private readonly List<ExhibitionContent> _contents = new List<ExhibitionContent>();
+        private AudioContent _audioContentEn;
+        private VideoContent _videoContentEn;
+        private TextContent _textContentEn;
+        private ImageContent _imageContentEn;
+
+        private AudioContent _audioContentFr;
+        private VideoContent _videoContentFr;
+        private TextContent _textContentFr;
+        private ImageContent _imageContentFr;
+
+        private readonly List<ExhibitionContent> _contentsEnglish = new List<ExhibitionContent>();
+        private readonly List<ExhibitionContent> _contentsFrench = new List<ExhibitionContent>();
+
         private List<ExhibitionContent> _expected;
         private PointOfInterest _poi;
 
@@ -39,7 +47,7 @@ namespace UnitTests
             _tdgEn.Db.DeleteAll<ExhibitionContentEn>();
             _tdgFr.Db.DeleteAll<ExhibitionContentFr>();
 
-            _audioContent = new AudioContent
+            _audioContentEn = new AudioContent
             {
                 Id = 1,
                 PoiId = 1,
@@ -51,7 +59,7 @@ namespace UnitTests
                 Encoding = "AudioEncoding"
             };
 
-            _videoContent = new VideoContent
+            _videoContentEn = new VideoContent
             {
                 Id = 2,
                 PoiId = 1,
@@ -63,7 +71,7 @@ namespace UnitTests
                 Encoding = "VideoEncoding"
             };
 
-            _textContent = new TextContent
+            _textContentEn = new TextContent
             {
                 Id = 3,
                 PoiId = 1,
@@ -73,7 +81,7 @@ namespace UnitTests
                 HtmlContent = "TheContentEn"
             };
 
-            _imageContent = new ImageContent
+            _imageContentEn = new ImageContent
             {
                 Id = 4,
                 PoiId = 1,
@@ -85,15 +93,66 @@ namespace UnitTests
                 Height = 250
             };
 
-            _contents.Add(_audioContent);
-            _contents.Add(_videoContent);
-            _contents.Add(_textContent);
-            _contents.Add(_imageContent);
+            _audioContentFr = new AudioContent
+            {
+                Id = 1,
+                PoiId = 1,
+                Title = "AudioTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                FilePath = "AudioPath",
+                Duration = 1,
+                Encoding = "AudioEncoding"
+            };
+
+            _videoContentFr = new VideoContent
+            {
+                Id = 2,
+                PoiId = 1,
+                Title = "VideoTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                FilePath = "VideoPath",
+                Duration = 1,
+                Encoding = "VideoEncoding"
+            };
+
+            _textContentFr = new TextContent
+            {
+                Id = 3,
+                PoiId = 1,
+                Title = "TextTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                HtmlContent = "TheContentEn"
+            };
+
+            _imageContentFr = new ImageContent
+            {
+                Id = 4,
+                PoiId = 1,
+                Title = "ImageTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                FilePath = "ImagePath",
+                Width = 250,
+                Height = 250
+            };
+
+            _contentsEnglish.Add(_audioContentEn);
+            _contentsEnglish.Add(_videoContentEn);
+            _contentsEnglish.Add(_textContentEn);
+            _contentsEnglish.Add(_imageContentEn);
+
+            _contentsFrench.Add(_audioContentFr);
+            _contentsFrench.Add(_videoContentFr);
+            _contentsFrench.Add(_textContentFr);
+            _contentsFrench.Add(_imageContentFr);
 
             _poi = new PointOfInterest
             {
                 Id = 1,
-                ExhibitionContent = _contents
+                ExhibitionContent = _contentsEnglish
             };
         }
 
@@ -104,49 +163,125 @@ namespace UnitTests
         }
 
         [Test]
-        public void AddExhibitionContentTest()
+        public void AddExhibitionContentEnTest()
         {
             _user.Language = Language.En;
-            _instance.AddExhibitionContent(_contents);
+            _instance.AddExhibitionContent(_contentsEnglish);
             _expected = _instance.GetExhibitionByPoiId(_poi.Id);
-            Assert.True(ExhibitionContent.ListEquals(_contents, _expected));
+            Assert.True(ExhibitionContent.ListEquals(_contentsEnglish, _expected));
+        }
+
+        public void AddExhibitionContentFrTest()
+        {
+            _user.Language = Language.Fr;
+            _instance.AddExhibitionContent(_contentsFrench);
+            _expected = _instance.GetExhibitionByPoiId(_poi.Id);
+            Assert.True(ExhibitionContent.ListEquals(_contentsFrench, _expected));
         }
 
         [Test]
-        public void UpdateExhibitionContentTest()
+        public void UpdateExhibitionContentEnTest()
         {
             _user.Language = Language.En;
 
-            _imageContent = new ImageContent
+            _audioContentEn = new AudioContent
             {
                 Id = 5,
                 PoiId = 1,
-                Title = "ImageTitle5",
+                Title = "AudioTitle",
                 Language = Language.En,
                 StorylineId = 1,
-                FilePath = "ImagePath5",
-                Width = 250,
-                Height = 250
+                FilePath = "AudioPath",
+                Duration = 1,
+                Encoding = "AudioEncoding"
             };
 
-            _contents.Add(_imageContent);
+            _videoContentEn = new VideoContent
+            {
+                Id = 6,
+                PoiId = 1,
+                Title = "VideoTitle",
+                Language = Language.En,
+                StorylineId = 1,
+                FilePath = "VideoPath",
+                Duration = 1,
+                Encoding = "VideoEncoding"
+            };
 
-            _instance.UpdateExhibitionList(_contents);
+            List<ExhibitionContent> contents2 = new List<ExhibitionContent>();
+            contents2.Add(_audioContentEn);
+            contents2.Add(_videoContentEn);
+
+            _instance.AddExhibitionContent(contents2);
+
+            contents2[1].Title = "TitleUpdated";
+            _instance.UpdateExhibitionList(contents2);
+
             _expected = _instance.GetExhibitionByPoiId(_poi.Id);
-            Assert.AreEqual(5, _expected.Count);
+            Assert.AreEqual("TitleUpdated", _expected[1].Title);
         }
 
         [Test]
-        public void ExhibitionContentModelToTableTest()
+        public void UpdateExhibitionContentFrTest()
         {
-           
+            _user.Language = Language.Fr;
+
+            _audioContentFr = new AudioContent
+            {
+                Id = 5,
+                PoiId = 1,
+                Title = "AudioTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                FilePath = "AudioPath",
+                Duration = 1,
+                Encoding = "AudioEncoding"
+            };
+
+            _videoContentFr = new VideoContent
+            {
+                Id = 6,
+                PoiId = 1,
+                Title = "VideoTitle",
+                Language = Language.Fr,
+                StorylineId = 1,
+                FilePath = "VideoPath",
+                Duration = 1,
+                Encoding = "VideoEncoding"
+            };
+
+            List<ExhibitionContent> contents3 = new List<ExhibitionContent>();
+            contents3.Add(_audioContentFr);
+            contents3.Add(_videoContentFr);
+
+            _instance.AddExhibitionContent(contents3);
+
+            contents3[1].Title = "TitleUpdated";
+            _instance.UpdateExhibitionList(contents3);
+
+            _expected = _instance.GetExhibitionByPoiId(_poi.Id);
+            Assert.AreEqual("TitleUpdated", _expected[1].Title);
+        }
+
+        [Test]
+        public void ConvertToAndFromTableFrTest()
+        {
+            List<ExhibitionContentFr> contentFrTable = _instance.ConvertToTableFr(_contentsFrench);
+            List<ExhibitionContent> contentFrModel = _instance.ConvertFromTableFr(contentFrTable);
+
+            Assert.AreEqual(_contentsFrench, contentFrModel);
         }
 
         [Test()]
-        public void ExhibitionContentTableToModelTest()
+        public void ConvertToAndFromTableEnTest()
         {
-            
+            List<ExhibitionContentEn> contentEnTable = _instance.ConvertToTableEn(_contentsEnglish);
+            List<ExhibitionContent> contentEnModel = _instance.ConvertFromTableEn(contentEnTable);
+
+            Assert.AreEqual(_contentsEnglish, contentEnModel);
+
         }
+
 
     }
 }
