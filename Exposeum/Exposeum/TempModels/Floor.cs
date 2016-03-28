ea0c5b48b9446ff07594edc2bf3438a6c1e83df6
Andrea <andrea18fp@hotmail.com>
2016-03-27 20:15:36 -0400

    Removed Icon table. Modified Floor class - saving image path as a string - and updated corresponding table/tdg/mapper/tests

--- Exposeum/Exposeum/TempModels/Floor.cs
+++ Exposeum/Exposeum/TempModels/Floor.cs
@@ -1,11 +1,14 @@
+using System.Runtime.ConstrainedExecution;
 using Android.Graphics;
+using Android.Graphics.Drawables;
 
 namespace Exposeum.TempModels
 {
     public class Floor
     {
         public int Id { get; set; }
-        public string Plan { get; set; } 
+        public string ImagePath { get; set; }
+        public Drawable FloorPlan { get; set; }
         public Paint Paint { get; set; }
 
         public Floor()
@@ -16,7 +19,7 @@ namespace Exposeum.TempModels
         public override bool Equals(object obj)
         {
             Floor other = (Floor) obj;
-            return Id == other.Id && Plan == other.Plan;
+            return Id == other.Id && ImagePath.Equals(other.ImagePath);
         }
     }
 }
