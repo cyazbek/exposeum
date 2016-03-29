e072ee7ca46b6d282f5b0b7135ca730d7f1e6bcb
Maxwell Mowbray <mmowbray@mentlegen.com>
2016-03-29 13:25:57 -0400

    EX-150 Removed floors assets because they are now being downloaded and stored

--- Exposeum/Exposeum/MapJSONParser.cs
+++ Exposeum/Exposeum/MapJSONParser.cs
@@ -16,13 +16,14 @@ namespace Exposeum
 	public class MapJSONParser
 	{
 
-		private static String JSON_BASE_URL = "http://mowbray.tech/exposeum";
-		private static String JSON_SCHEMA_FILENAME = "Map.json";
+		private readonly String JSON_BASE_URL = "http://mowbray.tech/exposeum";
+		private readonly String JSON_SCHEMA_FILENAME = "Map.json";
+
+		private readonly String LOCAL_VENUE_DATA_PATH = Environment.GetFolderPath (Environment.SpecialFolder.Personal) + "/venue_data";
 
 		private List<Floor> floors;
 		private List<MapElement> mapelements;
-		List<Storyline> storylines;
-
+		private List<Storyline> storylines;
 
 		public async void FetchAndParseMapJSON(){
 
@@ -53,12 +54,7 @@ namespace Exposeum
 				newFloor.Plan = (String)floorOBJ ["imagePath"];
 				newFloor.Id = int.Parse ((String)floorOBJ ["floorID"]);
 
-				///
-
-
-				BitmapDrawable floorImage = await DownloadImage (JSON_BASE_URL + newFloor.Plan);
-
-				///
+				BitmapDrawable floorImage = await SaveAndGetImage (newFloor.Plan);
 
 				floors.Add (newFloor);
 			}
@@ -89,20 +85,20 @@ namespace Exposeum
 
 			foreach (var storylineOBJ in JSONPayload["storyline"]) {
 
-				int storylineID = int.Parse ((String)storylineOBJ ["id"]);
-
 				Storyline newStoryline = new Storyline ();
+
+				int storylineID = int.Parse ((String)storylineOBJ ["id"]);
 
 				//TODO: associate previously-parsed MapElements to this storyline
 
 				String newStoryLineDescrition = (String)storylineOBJ ["description"];
+
 				storylines.Add (newStoryline);
 			}
 		}
 
 		private async Task<String> DownloadJSONAsync(string url)
 		{
-			
 			using (HttpClient client = new HttpClient())
 			using (HttpResponseMessage response = await client.GetAsync(url))
 			{
@@ -111,20 +107,29 @@ namespace Exposeum
 			}
 		}
 
-		private async Task<BitmapDrawable> DownloadImage(string url)
+		//download, save, and return the insance of the image
+		private async Task<BitmapDrawable> SaveAndGetImage(string path)
 		{
 			BitmapDrawable image = new BitmapDrawable ();
 
 			using (HttpClient client = new HttpClient())
-			using (HttpResponseMessage response = await client.GetAsync(url))
+			using (HttpResponseMessage response = await client.GetAsync(JSON_BASE_URL + path))
 			{
-				//response.EnsureSuccessStatusCode ();
+				response.EnsureSuccessStatusCode ();
 
-				using (var inputStream = await response.Content.ReadAsStreamAsync())
-				{
-					image = (BitmapDrawable)BitmapDrawable.CreateFromStreamAsync(inputStream, String.Empty).Result;
-				}
-	
+				var inputStream = await response.Content.ReadAsByteArrayAsync ();
+
+				string localPath = LOCAL_VENUE_DATA_PATH + path;
+
+				System.IO.Directory.CreateDirectory (Path.GetDirectoryName (localPath)); //create folder if it doesn't already exist
+
+				FileStream fs = new FileStream (localPath, FileMode.Create);
+
+				await fs.WriteAsync (inputStream, 0, inputStream.Length); //write the downloaded data to the device
+				fs.Close ();
+
+				image = (BitmapDrawable)BitmapDrawable.CreateFromPath (localPath); //instanciate the drawable from the just-saved data
+
 				return image;
 			}
 		}
