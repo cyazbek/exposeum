8dd56d9a83aa2d0921a71b6ac7fcd1d0db797337
Chris <christopher.yazbek@gmail.com>
2016-04-10 00:38:39 -0400

    Disabled Back after Walkthrough Activity

--- Exposeum/Exposeum/VisitActivity.cs
+++ Exposeum/Exposeum/VisitActivity.cs
@@ -10,7 +10,8 @@ using Java.IO;
 
 namespace Exposeum
 {
-    [Activity(Label = "Choose your Tour", Theme = "@style/CustomActionBarTheme", ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "Choose your Tour", Theme = "@style/CustomActionBarTheme",
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class VisitActivity : Activity
     {
         readonly User _user = User.GetInstance();
@@ -47,6 +48,7 @@ namespace Exposeum
             _actionBarTitle = FindViewById<TextView>(Resource.Id.TitleActionBar);
             _actionBarTitle.Text = _user.GetButtonText("TourModeTitle");
 
+
             //==========================================================================================================
             /*
             var BarButton = FindViewById<Button>(Resource.Id.butonn);
@@ -71,37 +73,41 @@ namespace Exposeum
             _freeVisitButton.Click += (sender, e) =>
             {
                 ExposeumApplication.IsExplorerMode = true;
-				ExplorerController.GetInstance().InitializeExplorerMode();
+                ExplorerController.GetInstance().InitializeExplorerMode();
 
-                var intent = new Intent(this, typeof(MapActivity));
+                var intent = new Intent(this, typeof (MapActivity));
                 StartActivity(intent);
 
             };
             _storylineButton.Click += (sender, e) =>
             {
-				ExposeumApplication.IsExplorerMode = false;
-				var intent = new Intent(this, typeof(StoryLineListActivity));
-				StartActivity(intent);
+                ExposeumApplication.IsExplorerMode = false;
+                var intent = new Intent(this, typeof (StoryLineListActivity));
+                StartActivity(intent);
 
             };
         }
 
+        public override void OnBackPressed()
+        {
+            return;
+        }
 
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Layout.MenuExplorer, menu);
             return base.OnCreateOptionsMenu(menu);
-            
+
         }
-        
+
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
-            
+
             //Toast.MakeText(this, txt, ToastLength.Long);
             switch (item.ItemId)
             {
-               
+
                 case Resource.Id.LanguageItem:
                     User.GetInstance().ToogleLanguage();
                     _freeVisitButton.Text = _user.GetButtonText("freeTour");
@@ -111,7 +117,7 @@ namespace Exposeum
                     return true;
 
                 case Resource.Id.QRScannerItem:
-					QrController.GetInstance(this).BeginQrScanning();
+                    QrController.GetInstance(this).BeginQrScanning();
                     return true;
             }
             return base.OnOptionsItemSelected(item);
@@ -120,10 +126,10 @@ namespace Exposeum
         public override bool OnPrepareOptionsMenu(IMenu menu)
         {
             var menuItem1 = menu.GetItem(0).SetTitle(_user.GetButtonText("LanguageItem"));
-            var menuItem2= menu.GetItem(1).SetTitle(_user.GetButtonText("QRScannerItem"));
-            return true; 
+            var menuItem2 = menu.GetItem(1).SetTitle(_user.GetButtonText("QRScannerItem"));
+            return true;
 
         }
-        
     }
+
 }
