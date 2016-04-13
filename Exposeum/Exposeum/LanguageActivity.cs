78074c8fe42342c9674bc39bf26270af5feb016b
Chris <christopher.yazbek@gmail.com>
2016-04-13 01:19:11 -0400

    Language selection UI mostly done.

--- Exposeum/Exposeum/LanguageActivity.cs
+++ Exposeum/Exposeum/LanguageActivity.cs
@@ -3,6 +3,7 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Android.Webkit;
 using Android.Widget;
 using Exposeum.Models;
 
@@ -22,9 +23,18 @@ namespace Exposeum
             this.Window.AddFlags(WindowManagerFlags.Fullscreen);
             SetContentView(Resource.Layout.Language);
 
+
+
+            WebView gifView = FindViewById<WebView>(Resource.Id.gifView);
+
+            gifView.LoadDataWithBaseURL("file:///android_asset/",
+                "<style>img{display: inline;max-height: 100%;max-width: 100%;}</style>"+
+                "<html><body><img src=\"languageBanner.gif\" ></body></html>", "text/html", "utf-8", null);
+
+
             var langList = new string[]
             {
-                "Choose your Language","Francais", "English"
+                "","Francais", "English"
             };
             spinnerLang = FindViewById<Spinner>(Resource.Id.spinnerLang);
             spinnerLang.Adapter = new ArrayAdapter<string>(this, 
